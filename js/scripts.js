383f17a395d65fb08e90c696087ccb92f80a5364
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-04-21 00:00:28 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".clickable-1").click(function() {
+    $("#showing-1").toggle();
+    $("#hidden-1").toggle();
+  });
+  $(".clickable-2").click(function() {
+    $("#showing-2").toggle();
+    $("#hidden-2").toggle();
+  });
+  $(".clickable-3").click(function() {
+    $("#showing-3").toggle();
+    $("#hidden-3").toggle();
+  });
+  $(".clickable-4").click(function() {
+    $("#showing-4").toggle();
+    $("#hidden-4").toggle();
+  });
+});
